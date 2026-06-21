3b17eeb19bc037f09f3f3915e138def74803cca0
Nguyen Vu An <nguyenvuan2005@gmail.com>
2026-06-21 16:45:48 +0700

    Update videos.ts

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,25 @@
-import { respondWithJSON } from "./json";
+ import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+ import {getBearerToken, validateJWT} from "../auth.ts";
+ import {getVideo, getVideos} from "../db/videos.ts";
+ import {BadRequestError} from "./errors.ts";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const upload_limit = 1 << 30; // 1GB
+  const videoId = req.params as string;
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const videoMetaData = getVideo(cfg.db, videoId);
+  if (videoMetaData?.userID !== userID) {
+    throw new BadRequestError("You are not authorized to upload this video");
+  }
+
+  const videoFile = (await req.formData()).get("video") as File;
+
+
+
   return respondWithJSON(200, null);
 }
